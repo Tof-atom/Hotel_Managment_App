4a9aeac72998deb3cdc93fd13ea09a32ef7853d4
Tof-atom <toufel@inbox.ru>
2022-10-25 17:26:20 -0400

    Register, Login in process

--- Authentication/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Authentication/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -55,7 +55,7 @@ namespace Authentication {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Authentication;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Authentication;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
